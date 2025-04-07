53e3e5414b46f43921f7bce27b0b9eeffc0a8d0e
Martin Jonasson <grapefrukt@grapefrukt.com>
2025-04-07 14:58:35 +0200

    actually using the custom args too

--- reports/title.py
+++ reports/title.py
@@ -6,29 +6,22 @@ class ReportTitle(Report):
 
     @property
     def name(self) -> str:
-        return 'title'
+        return 'custom'
     
     def generate(self, months, platforms, df: pd.DataFrame) -> str:
-        df = df.groupby(['title'])
+        df = df.groupby(self.arguments)
         df = df.agg({
             'units': 'sum',
             'sek': 'sum',
         })
 
-        df = df.sort_values(['title'], ascending=True)
+        df = df.sort_values(self.arguments, ascending=True)
         df = df.reset_index()
 
-        df.loc['total']= df.sum(numeric_only = True)
-
+        df.loc['total'] = df.sum(numeric_only = True)
+        df.loc['total'] = df.loc['total'].fillna('')
+        
         df['units'] = df['units'].map(lambda a: self.format_units(a))
         df['sek'] = df['sek'].map(lambda a: self.format_currency_decimals(a))
 
         print(df)
-
-    def report_row(self, title, units, sek):
-        if not isinstance(units, str):
-            units = self.format_units(units)
-        if not isinstance(sek, str):
-            sek = self.format_currency_decimals(sek)
-
-        return f'{title:<28}{units:>10}{sek:>20}\n'
